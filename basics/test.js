7e91ac16055081919b911899be5d0de68a36fb8a
Vavaldas-jaiwanth <150772769+Vavaldas-jaiwanth@users.noreply.github.com>
2023-11-18 10:16:28 +0000

    commit

--- basics/test.js
+++ basics/test.js
@@ -1,4 +1,8 @@
-let k=8298317
-let ans=Boolean(k)
-console.log(typeof(ans))
-console.log(ans)
+// let k=8298317
+// let ans=Boolean(k)
+// let s=String(ans)
+// console.log(typeof(s))
+// console.log(s)
+
+//console.log(undefined>=0);
+console.log("a"<"bc");
