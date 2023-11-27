0354f89ede820417de32f6e789dc206f94b258a7
Vavaldas-jaiwanth <150772769+Vavaldas-jaiwanth@users.noreply.github.com>
2023-11-27 13:40:59 +0000

    commit

--- basics/test.js
+++ basics/test.js
@@ -5,4 +5,21 @@
 // console.log(s)
 
 //console.log(undefined>=0);
-console.log("a"<"bc");
+//console.log("a"<"bc");
+// ---- Array concatinaton--//
+// const a=[1,2,3]
+// const b=[4,5,6]
+// a.push(b)
+// console.log(a);//out put --[1 ,2 ,3,[4 5 6]]
+// const c=a.concat(b)
+// console.log(c);//out put - [1,2,3,4,5,6]
+// alternate//
+// const d=[...a,...b]
+// console.log(d);output -[1,2,3,4,5,6] (...)is the spread operator can be used to concat multiple arrays
+
+// const a=[1,2,3,4,[5,6,7,8],[[9]]]
+// // flat takes the argumenst as number of dimensions of array zero indexed used to reduct multy dim arry t single
+// const ans=a.flat()
+// console.log(ans);
+//console.log(Array.from("jaiwanth"));//creates an array from iterable object
+//console.log(Array.of(1,2,3,4));//creates an array from element
