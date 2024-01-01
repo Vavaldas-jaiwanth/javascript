b6429553094a20b23dcc82c1842cb270d383fde8
Vavaldas-jaiwanth <150772769+Vavaldas-jaiwanth@users.noreply.github.com>
2024-01-01 07:49:51 +0000

    commit-Dom

--- basics/test2.js
+++ basics/test2.js
@@ -42,22 +42,65 @@
 // console.log(name);
 ///-Json formater -understanding of api responses
 ///////-Functions-///////
-const k=function add(num1,num2){
-    return (num1+num2);
-}
-//undefined s
-//"",undefined ,null are taken as false
-function solve(s)
-{
-    if(!null)
-    console.log("null");
+// const k=function add(num1,num2){
+//     return (num1+num2);
+// }
+// //undefined s
+// //"",undefined ,null are taken as false
+// function solve(s)
+// {
+//     if(!null)
+//     console.log("null");
 
-    return `hi ${s} `
-}
-console.log(solve());
+//     return `hi ${s} `
+// }
+// console.log(solve());
 //default value
 // function solve(s="jai")
 // {
 //     return `hi ${s} `
 // }
 // console.log(solve());
+
+
+///----DOM------
+
+//getting elements
+/*
+document.getElementById("title")
+attributes
+document.getElementById("tittle").id || document.getElementById("tittle").getAttribute("id")
+class
+document.getElementById("tittle").className
+
+setattribute
+document.getElementById("tittle").setAttribute("class","test")
+
+css through console
+title.style.backgroundColor="greeen"
+
+extracting text
+1-tittle.textContent
+2-tittle.innerHTML(given the html under the tag)
+3-tittle.innerText
+
+QUERYSELECTORS
+document.queryselector("h1")-for tag
+document.queryselector("#title")-for id
+document.queryselector(".heading")-for class 
+
+Queryselector return a NodeList
+const temp=documennt.querySelectorAll("h1")
+
+temp[0].style.color="green"
+
+foreach syntax
+
+temp.forEach((k)=>{
+    k.syle.color="green"
+})
+
+type conversion from html collection to array
+Array.from(temp)
+
+*/ 
