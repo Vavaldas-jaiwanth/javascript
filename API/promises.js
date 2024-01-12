2a32405f20bb12e35b475c106e81acecd93a60e0
jaiwanth <jaiwanthvavaldas@gmail.com>
2024-01-12 23:22:43 +0530

    promises

--- API/promises.js
+++ API/promises.js
@@ -1,35 +1,35 @@
-// const promisone= new Promise((resolve,reject)=>{
-//     setTimeout(function(){
-//         console.log("promise triggered");
-//         resolve();
-//     },1000)
-// })
+const promisone= new Promise((resolve,reject)=>{
+    setTimeout(function(){
+        console.log("promise triggered");
+        resolve();
+    },1000)
+})
 
-// promisone.then(function(){
-//     console.log("resolve called");
-// })
+promisone.then(function(){
+    console.log("resolve called");
+})
 
-// new Promise((resolve,reject)=>{
-//     setTimeout(function(){
-//         console.log("promise 2 triggered");
+new Promise((resolve,reject)=>{
+    setTimeout(function(){
+        console.log("promise 2 triggered");
 
-//         resolve();
-//     },1000);
-// }).then(function (data) {
-//     console.log("resolve2 called");
+        resolve();
+    },1000);
+}).then(function (data) {
+    console.log("resolve2 called");
     
-// })
+})
 
-// const promisthree= new Promise((resolve,reject)=>{
-//     setTimeout(function(){
-//         console.log("promise3 triggered");
-//         resolve({name:"jaiwanth",age:20});
-//     },1000)
-// })
+const promisthree= new Promise((resolve,reject)=>{
+    setTimeout(function(){
+        console.log("promise3 triggered");
+        resolve({name:"jaiwanth",age:20});
+    },1000)
+})
 
-// promisthree.then(function(data){
-//     console.log(data);
-// })
+promisthree.then(function(data){
+    console.log(data);
+})
 
 const promisfour= new Promise((resolve,reject)=>{
    setTimeout(function () {
@@ -46,40 +46,41 @@ const promisfour= new Promise((resolve,reject)=>{
    },1000);
 })
 //a chain of then ,the "data" of the nxt then is the data returned by the previous then 
-// promisfour
-// .then(function(data){
-//     console.log(data);
+promisfour
+.then(function(data){
+    console.log(data);
 
-//     return data.status;
-// })
-// .then(function(data){
-//     console.log(data);
-// })
-// .catch(function(err){
-//     console.log(err);
-// })
-// .finally(()=>{
-//     console.log("finally executed");
-// })
+    return data.status;
+})
+.then(function(data){
+    console.log(data);
+})
+.catch(function(err){
+    console.log(err);
+})
+.finally(()=>{
+    console.log("finally executed");
+})
 
 console.log(promisfour);
 
 //promises using async and await
 
-// async function consumePromise(){
-//     const res=await promisfour;
-//     console.log(res);
+async function consumePromise(){
+    const res=await promisfour;
+    console.log(res);
 
-// }
-// consumePromise()
+}
+consumePromise()
 
 //this is the async and await method
-// async function getusers(){
-//     const res= await fetch('https://api.github.com/users/hiteshchoudhary');
-//     const data =await res.json();
-//     console.log(data);
-// }
-// getusers()
+
+async function getusers(){
+    const res= await fetch('https://api.github.com/users/hiteshchoudhary');
+    const data =await res.json();
+    console.log(data);
+}
+getusers()
 
 //using .then
 
